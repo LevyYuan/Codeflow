5282f698f6694517d561d97016d4169b9e36fd3c
LevyYuan <1023750831@qq.com>
2026-02-06 16:46:46 +0800

    优化

--- vite.config.ts
+++ vite.config.ts
@@ -45,6 +45,7 @@ export default defineConfig((config) => {
       },
       config.mode === 'development' && remixCloudflareDevProxy(),
       remixVitePlugin({
+        ssr: false,
         future: {
           v3_fetcherPersist: true,
           v3_relativeSplatPath: true,
