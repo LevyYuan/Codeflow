3a09be79e00914080b3ea2ab2f199724f818dfbc
LevyYuan <1023750831@qq.com>
2026-02-06 18:28:00 +0800

    feat: add Node.js server support for self-hosting

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
-import { createRequestHandler } from "@remix-run/vercel";
-import * as build from "@remix-run/dev/server-build";
+const path = require('path');
+const express = require('express');
+const { createRequestHandler } = require('@remix-run/express');
 
-export default createRequestHandler({ build });
+const app = express();
+
+// 静态文件
+app.use(express.static('build/client'));
+
+// Remix 路由
+app.all(
+  '*',
+  createRequestHandler({
+    build: require('./build/server/index.js'),
+    mode: process.env.NODE_ENV,
+  })
+);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
